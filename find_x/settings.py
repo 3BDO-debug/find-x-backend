19dd3d067f9fb8098d47c16a177a35c4671f99a6
Abdelrahman Fahmy <rooted39@gmail.com>
2021-12-31 10:33:41 +0200

    final config

--- find_x/settings.py
+++ find_x/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["find-x-beta.herokuapp.com"]
+ALLOWED_HOSTS = ["find-x-beta.herokuapp.com", "127.0.0.1"]
 
 
 # Custome auth model
